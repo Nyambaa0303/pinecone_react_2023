d3ea248f5fcba505c82c55eeb7e54f5f117f46a2
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-01-03 22:03:44 +0800

    u

--- src/main.js
+++ src/main.js
@@ -1,17 +1,37 @@
 import Card from "react-bootstrap/Card";
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Modal from "react-bootstrap/Modal";
+import Modal from 'react-bootstrap/Modal';
+
 function Main() {
+
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
-      <div className="col-6 mx-auto mt-5 ">
+      <div className="col-6 mx-auto mt-5 d-flex justify-content-between">
         <h2>Ангилал</h2>
+        <Button variant="outline-primary" onClick={handleShow}>Шинэ</Button>
       </div>
       <Card className="col-6 my-5 mx-auto">
         <Card.Body className=" ">Улс төр</Card.Body>
       </Card>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
