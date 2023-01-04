85e5aa038b9903addcc06c7255afcba755cf92bc
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-01-04 17:23:55 +0800

    end

--- Admin-panel/src/header.js
+++ Admin-panel/src/header.js
@@ -9,7 +9,7 @@ function Header() {
   return (
     <Navbar bg="dark" expand="lg">
       <Container>
-        <img src={logo} className="App-logo d-sm-none" alt="logo" />
+        <img src={logo} className="App-logo d-none d-lg-block" alt="logo" />
         <Navbar.Brand href="#home" className="text-light">
           Админ
         </Navbar.Brand>
@@ -33,10 +33,7 @@ function Header() {
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
-        <Button
-          variant="dark"
-          className="bg-white text-dark d-sm-none d-lg-block"
-        >
+        <Button variant="dark" className="bg-white text-dark d-none d-lg-block">
           Гарах
         </Button>
       </Container>
