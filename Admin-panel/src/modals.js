a38a347715d6506e3af12f87c8c0fe0c345c6f89
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-01-05 17:50:14 +0800

    u

--- Admin-panel/src/modals.js
+++ Admin-panel/src/modals.js
@@ -4,39 +4,6 @@ import Form from "react-bootstrap/Form";
 import React from "react";
 import Button from "react-bootstrap/Button";
 
-// function Modals() {
-//   return (
-//     <Modal show={show} onHide={handleClose}>
-//       <Modal.Header closeButton>
-//         <Modal.Title>Ангилал нэмэх</Modal.Title>
-//       </Modal.Header>
-//       <Modal.Body>
-//         <InputGroup className="mb-1">
-//           <b>Hэр</b>
-//         </InputGroup>
-//         <Form.Control
-//           aria-label="Default"
-//           aria-describedby="inputGroup-sizing-default"
-//           placeholder=" Ангилалийн нэр"
-//         />
-//       </Modal.Body>
-//       <Modal.Footer className=" d-flex justify-content-between">
-//         <Button
-//           variant="white"
-//           onClick={handleClose}
-//           className="btn btn-outline-danger  onClick={deleteThis}"
-//         >
-//           Устгах
-//         </Button>
-//         {/* <AwesomeButton type="danger">Устгах</AwesomeButton> */}
-
-//         <Button variant="primary">Хадгалах</Button>
-//       </Modal.Footer>
-//     </Modal>
-//   );
-// }
-// export default Modals;
-
 export function Modals({ show, onClose }) {
   return (
     <>
