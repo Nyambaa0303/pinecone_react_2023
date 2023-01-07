cbd2b0eab6e38362079d3a389644a25989ed96ed
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-01-07 22:41:59 +0800

    todo

---
+++ todo-list/src/time.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+
+class Time extends React.Component {
+  state = {
+    date: "",
+  };
+
+  componentDidMount() {
+    this.getDate();
+  }
+
+  getDate = () => {
+    var today = new Date(),
+      date =
+        today.getFullYear() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getDate();
+
+    this.setState({ date });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p className="text-secondary">
+          Current Date (Y-m-d) : {this.state.date}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Time;
--- todo-list/src/todo.js
+++ todo-list/src/todo.js
@@ -7,6 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BsCheckSquareFill } from "react-icons/bs";
 import { BsFillTrashFill } from "react-icons/bs";
 import { BsPenFill } from "react-icons/bs";
+import Time from "./time";
 
 export function Todos() {
   const [text, setText] = useState("");
@@ -37,28 +38,35 @@ export function Todos() {
   }
 
   return (
-    <div className="card1">
+    <div className="card1 pt-5 border rounded-4 border-warning mt-5 p-4 shadow-lg p-3 mb-5 bg-body-tertiary rounded">
       <h1>Todo List App</h1>
+      <Time />
 
       <InputGroup className="">
         <Form.Control
-          className="bg-light"
+          className="bg-light border-warning rounded-3 mt-5 text-secondary shadow p-3 mb-5 bg-body-tertiary rounded"
           placeholder="Add new task"
           value={text}
           style={{ borderColor: error ? "red" : "warning" }}
           onChange={handleTextChange}
         />
-        <Button onClick={addTodo}>Button</Button>
+        <Button
+          className="mt-5 btn rounded-3 shadow p-3 mb-5 bg-body-tertiary rounded"
+          variant="outline-warning"
+          onClick={addTodo}
+        >
+          Add++
+        </Button>
 
         {error && <div style={{ color: "red" }}>{error}</div>}
       </InputGroup>
 
       <div className="">
-        <h1>jagsaalt</h1>
+        <h1 className="mt-5 border-bottom border-4 pb-3">Task List</h1>
         {todos.map((todo, index) => {
           return (
             <div
-              className="d-flex justify-content-between bg-light border rounded-3 border-secondary"
+              className="d-flex justify-content-between bg-light border border-warning text-secondary rounded-3 border-secondary mt-5 align-items-center p-4 shadow p-3 mb-5 bg-body-tertiary rounded"
               key={index}
               style={{ textDecoration: "line-through" }}
             >
