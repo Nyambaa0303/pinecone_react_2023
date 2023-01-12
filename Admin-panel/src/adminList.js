547646eb23d82c8c9caff519f54d2e250a58fded
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-01-12 21:05:33 +0800

    u

--- Admin-panel/src/adminList.js
+++ Admin-panel/src/adminList.js
@@ -11,15 +11,15 @@ export function AdminList() {
       {list.map((card) => (
         <div
           key={card.id}
-          className=" border rounded-3 p-4 mt-5 col-6 mx-auto  d-flex justify-content-between align-items-center"
+          className="row border rounded-3 p-4 mt-5 col-6 mx-auto  d-flex justify-content-between align-items-center"
         >
-          <b>{card.name}</b>
-          <div className="d-flex gap-1">
+          <b className="col-none-12 col-lg-6">{card.name}</b>
+          <div className="d-flex gap-1 col-none-12 col-lg-6 justify-content-center">
             <Button variant="outline-secondary">Засах</Button>
+            <Button variant="outline-primary">Хийсэн</Button>
             <Button variant="white" className="btn btn-outline-danger">
               Устгах
             </Button>
-            <Button variant="outline-primary">Хийсэн</Button>
           </div>
         </div>
       ))}
