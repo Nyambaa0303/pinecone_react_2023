f6763a5c2534ecc028221ed42f9a03db78a40d6a
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-01-20 12:53:41 +0800

    U

--- blog-web/src/component/admin/AdminApp.js
+++ blog-web/src/component/admin/AdminApp.js
@@ -3,16 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import { Home } from "./Home";
 import { Header } from "./Header";
+import { Particless } from "../Particles";
 
 export function AdminApp() {
   return (
     <>
+      <Particless />
       <Header />
       <Container>
-        <div style={{ maxWidth: 700, margin: "2rem auto" }}>
+        <div style={{ maxWidth: 700, margin: "2rem auto", position: "fixed" }}>
           <Routes>
             <Route path="/home" element={<Home />} />
-            <Route path="/categories" element={<div>Categories app</div>} />
+            <Route
+              path="/categories"
+              element={<div style={{ color: "white" }}>Categories app</div>}
+            />
             <Route path="/about" element={<div>About app</div>} />
             <Route path="/contact" element={<div>Contact app</div>} />
           </Routes>
