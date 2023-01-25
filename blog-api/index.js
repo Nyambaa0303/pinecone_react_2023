7b9f4622094d980bbd704e05496725c9cc9f73aa
evening-2 <evening-2@admins-iMac-7.local>
2023-01-25 16:56:37 +0800

    test

--- blog-api/index.js
+++ blog-api/index.js
@@ -7,7 +7,14 @@ const app = express();
 app.use(cors());
 
 app.get("/", (request, response) => {
-  response.json([{ name: "Amka" }, { name: "Tsagaan" }]);
+  response.json([
+    { name: "Amka", age: 22 },
+    { name: "Tsagaan", age: 25 },
+    { name: "Nairaa", age: 20 },
+    { name: "Huslee", age: 20 },
+    { name: "Namuun", age: 24 },
+    { name: "Nyamka", age: 32 },
+  ]);
 });
 
 app.listen(port, () => {
