bca0fb281009bd493dd338d914af7c8292019823
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-01-27 13:08:47 +0800

    update

--- blog-api/index.js
+++ blog-api/index.js
@@ -1,22 +1,50 @@
 const express = require("express");
 const cors = require("cors");
+const { v4: uuid } = require("uuid");
 
-const port = 8000;
+const port = 4000;
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.json([
-    { name: "Amka", age: 22 },
-    { name: "Tsagaan", age: 25 },
-    { name: "Nairaa", age: 20 },
-    { name: "Huslee", age: 20 },
-    { name: "Namuun", age: 24 },
-    { name: "Nyamka", age: 32 },
-  ]);
+let data = [
+  {
+    id: uuid(),
+    name: "Amka",
+    age: "23",
+  },
+  {
+    id: uuid(),
+    name: "Tsagaan",
+    age: "25",
+  },
+  {
+    id: uuid(),
+    name: "Huslee",
+    age: "20",
+  },
+  {
+    id: uuid(),
+    name: "Nyamoo",
+    age: "32",
+  },
+];
+
+app.get("/data", (req, res) => {
+  res.json(data);
+});
+
+app.get("/data/:id", (req, res) => {
+  const { id } = req.params;
+  const one = data.find((datas) => datas.id === id);
+  if (one) {
+    res.json(one);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.listen(port, () => {
-  console.log("server is running ");
+  console.log(`Server is running on port ${port}`);
 });
